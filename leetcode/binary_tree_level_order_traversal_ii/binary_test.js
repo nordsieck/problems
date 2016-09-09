5e9e71a2afb67e286ffa679b293eddfc85e8b02b
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2016-09-09 04:22:46 -0700

    fix traversal (#12)

--- leetcode/binary_tree_level_order_traversal_ii/binary_test.js
+++ leetcode/binary_tree_level_order_traversal_ii/binary_test.js
@@ -1,7 +1,11 @@
+"use strict";
+
 function TreeNode(val, left, right) { [this.val, this.left, this.right] = [val, left, right]; }
 
 var levelOrderBottom = function(root) {
-    var [result, vals, next] = [[], [], [root]];
+    if (root === null) { return []; }
+
+    let [result, vals, next] = [[], [], [root]];
     
     while (next.length !== 0) {
 	[vals, next] = level(next);
@@ -11,9 +15,9 @@ var levelOrderBottom = function(root) {
 };
 
 function level(nodes) {
-    var [vals, next] = [[], []];
+    let [vals, next] = [[], []];
     
-    for (var i = 0; i < nodes.length; i++) {
+    for (let i = 0; i < nodes.length; i++) {
 	vals.push(nodes[i].val);
 	if (nodes[i].left !== null) { next.push(nodes[i].left); }
 	if (nodes[i].right !== null) { next.push(nodes[i].right); }
@@ -23,19 +27,20 @@ function level(nodes) {
 }
 
 function test(fn, nodes, expected) {
-    var [out, exp] = [JSON.stringify(fn(nodes)), JSON.stringify(expected)];
+    let [out, exp] = [JSON.stringify(fn(nodes)), JSON.stringify(expected)];
 
     if (out !== exp) { process.stdout.write("expected: " + exp + "\ngot:      " + out + "\n"); return false; }
     return true;
 }
 
 function Test() {
-    var failure = false;
-    for (i in arguments) { if (!arguments[i]) { failure = true; }}
+    let failure = false;
+    for (let i in arguments) { if (!arguments[i]) { failure = true; }}
     if (failure) { process.exit(1); }
 }
 
 Test(
+    test(levelOrderBottom, null, []),
     test(levelOrderBottom, new TreeNode(1, null, null), [[1]]),
     test(levelOrderBottom, new TreeNode(1, new TreeNode(2, null, null), new TreeNode(3, null, null)), [[2, 3], [1]]),
     test(level, [new TreeNode(1, null, null)], [[1], []]),
