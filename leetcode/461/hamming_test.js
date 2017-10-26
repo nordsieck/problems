69ccb45fcac2237a06f9844cbfd8314a8fc39f4a
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2017-10-26 06:49:57 -0700

    hamming distance (#461)

---
+++ leetcode/461/hamming_test.js
@@ -0,0 +1,24 @@
+"use strict";
+const test = require('../../test.js');
+
+var hammingDistance = function(x, y) { return bits(x ^ y); }
+
+function bits(num) {
+    let count = 0;
+    while (num > 0) {
+	if (num % 2 != 0) { count++; num -= 1; }
+	num /= 2;
+    }
+    return count;
+}
+
+test.Run(
+    test.test(bits(0), 0),
+    test.test(bits(1), 1),
+    test.test(bits(3), 2),
+    test.test(bits(1024), 1),
+    test.test(bits(1023), 10),
+
+    test.test(hammingDistance(1, 4), 2),
+    test.test(hammingDistance(1024, 1024), 0),
+);
