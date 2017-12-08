79548789f6bc512b3ee48bc7419077d3e63e5f92
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2017-12-07 16:19:46 -0800

    Add Two Numbers (#2)

--- leetcode/2/numbers.go
+++ leetcode/2/numbers.go
@@ -6,5 +6,27 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry := 0
+	one, two := l1, l2
+	var head *ListNode
+	var curr **ListNode = &head
 
+	for ; one != nil && two != nil; one, two, curr = one.Next, two.Next, &(*curr).Next {
+		*curr = &ListNode{Val: (one.Val + two.Val + carry) % 10}
+		carry = (one.Val + two.Val + carry) / 10
+	}
+
+	if one == nil {
+		one, two = two, one
+	}
+	for ; one != nil; one, curr = one.Next, &(*curr).Next {
+		*curr = &ListNode{Val: (one.Val + carry) % 10}
+		carry = (one.Val + carry) / 10
+	}
+
+	if carry == 1 {
+		*curr = &ListNode{Val: carry}
+	}
+
+	return head
 }
